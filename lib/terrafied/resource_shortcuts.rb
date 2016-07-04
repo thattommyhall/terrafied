da2404aab26d982e97b4c9b7bcd5ba30de6807c8
thattommyhall <thattommyhall@gmail.com>
2016-07-04 13:13:55 +0100

    Add deep_merge from active_support

--- lib/terrafied/resource_shortcuts.rb
+++ lib/terrafied/resource_shortcuts.rb
@@ -1,3 +1,14 @@
+class Hash
+  def deep_merge(other_hash)
+    self.merge(other_hash) do |key, oldval, newval|
+      oldval = oldval.to_hash if oldval.respond_to?(:to_hash)
+      newval = newval.to_hash if newval.respond_to?(:to_hash)
+      oldval.class.to_s == 'Hash' &&
+        newval.class.to_s == 'Hash' ? oldval.deep_merge(newval) : newval
+    end
+  end
+end
+
 class << self
   def aws_api_gateway_account(name, spec)
     resource 'aws_api_gateway_account', name, spec
