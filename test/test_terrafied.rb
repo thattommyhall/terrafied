8130b61303123b662353bcbee861e220d98aaa09
thattommyhall <thattommyhall@gmail.com>
2016-07-04 17:12:55 +0100

    Remove old tests

--- test/test_terrafied.rb
+++ test/test_terrafied.rb
@@ -5,17 +5,4 @@ class TerrafiedTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Terrafied::VERSION
   end
-
-  def test_it_does_something_useful
-    assert_equal({"provider" => {},
-                  "resource" =>
-                  {"type"=>{"name"=>{:spec=>true}}}},
-
-                 Terrafied.build do
-                   resource 'type', 'name', { spec: true }
-                 end)
-  end
-
-  def test_it_errors_on_same
-  end
 end
